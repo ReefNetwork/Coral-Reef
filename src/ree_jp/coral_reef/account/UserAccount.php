d9025aecfcb29d166634a02ff7a00c9c333098cc
Kondo Ryosuke <mail@ree-jp.net>
2021-07-25 02:48:19 +0900

    sql周りをちょっと追加

--- src/ree_jp/coral_reef/account/UserAccount.php
+++ src/ree_jp/coral_reef/account/UserAccount.php
@@ -8,16 +8,12 @@ class UserAccount
 {
     public string $xuid;
     public string $name;
-    public string $nowIp;
-    public array $ips;
-    public array $deviceIds;
+    public int $level;
+    public int $experiment;
 
-    public function __construct(string $xuid, string $name, string $nowIp, array $ips, array $deviceIds)
+    public function __construct(string $xuid, string $name, int $level, int $experiment)
     {
         $this->xuid = $xuid;
         $this->name = $name;
-        $this->nowIp = $nowIp;
-        $this->ips = $ips;
-        $this->deviceIds = $deviceIds;
     }
 }
--- src/ree_jp/coral_reef/sql/SQLManager.php
+++ src/ree_jp/coral_reef/sql/SQLManager.php
@@ -4,6 +4,7 @@
 namespace ree_jp\coral_reef\sql;
 
 
+use Exception;
 use PDO;
 use PDOException;
 use ree_jp\coral_reef\account\UserAccount;
@@ -12,7 +13,16 @@ class SQLManager
 {
     static SQLManager $manager;
 
+    /**
+     * USER :XUID:NAME:IPS:EXPERIMENT
+     * BAN :TYPE:VALUE:REASON:TIME
+     * WHITELIST :TYPE:VALUE:TIME
+     */
     private PDO $pdo;
+
+    /**
+     * XUID :TYPE:OTHER:VALUE:TIME
+     */
     private PDO $logPdo;
 
     /**
@@ -33,15 +43,39 @@ class SQLManager
         $this->pdo = new PDO($dsn, $user, $pass, $options);
         $logDsn = "mysql:host=$host;dbname=${dbName}Log;charset=utf8";
         $this->logPdo = new PDO($logDsn, $user, $pass, $options);
+        $this->createTable();
     }
 
     /**
      * Banされている時はその理由、されていないときはnull
      * @param string $xuid
+     * @param string $ip
      * @return string|null
+     * @throws Exception
      */
-    public function getBan(string $xuid): ?string
+    public function getBanReason(string $xuid, string $ip): ?string
     {
+        $collectQuery = $this->pdo->query("SELECT VALUE ,REASON FROM BAN WHERE TYPE = 'ALL'");
+        if ($collectQuery === false) return null;
+        $collectPrepare = $this->pdo->prepare("SELECT NAME FROM USER WHERE XUID = :xuid AND FIND_IN_SET(:ip ,IPS)");
+        while ($banData = $collectQuery->fetch(PDO::FETCH_ASSOC)) {
+            if (!(array_key_exists('XUID', $banData) && array_key_exists('REASON', $banData))) throw new Exception('BAN_SQLの返り値が不正です');
+
+            if ($xuid === $banData['XUID']) return $banData['REASON'];
+
+            $collectPrepare->execute([':xuid' => $banData['XUID'], ':ip' => $ip]);
+            $collectResult = $collectPrepare->fetchColumn();
+            if ($collectResult !== false) return $banData['REASON'];
+        }
+
+        $singlePrepare = $this->pdo->prepare("SELECT REASON FROM BAN WHERE 
+                            (TYPE = 'IP' AND VALUE = :ip ) OR 
+                            (TYPE = 'XUID' AND VALUE = :xuid )");
+        if ($singlePrepare === false) return null;
+        $singlePrepare->execute([':ip' => $ip, ':xuid' => $xuid]);
+        $prepareResult = $singlePrepare->fetchColumn();
+        if (!is_string($prepareResult)) return null;
+        return $prepareResult;
     }
 
     /**
@@ -52,8 +86,20 @@ class SQLManager
     {
     }
 
+    /**
+     * @throws Exception
+     */
+    private function createLogTable(string $xuid): void
+    {
+        $prepare = $this->logPdo->prepare("CREATE TABLE IF NOT EXISTS :xuid(TYPE ENUM('join','quit','warp','skill','break','place','chat','other') NOT NULL ,OTHER VARCHAR,VALUE VARCHAR NOT NULL ,TIME DATETIME NOT NULL )");
+        if ($prepare === false) throw new Exception('ログテーブルが作成できません');
+        $prepare->execute([':xuid' => $xuid]);
+    }
+
     private function createTable(): void
     {
-
+        $this->pdo->exec('CREATE TABLE IF NOT EXISTS USER (XUID BIGINT UNSIGNED NOT NULL PRIMARY KEY ,NAME VARCHAR(100) NOT NULL ,IPS VARCHAR(9999) NOT NULL ,EXPERIMENT BIGINT UNSIGNED NOT NULL )');
+        $this->pdo->exec("CREATE TABLE IF NOT EXISTS BAN (PRIMARY KEY (TYPE ,VALUE ),TYPE ENUM('ALL','XUID','IP') NOT NULL ,VALUE VARCHAR(20) NOT NULL ,REASON VARCHAR(999) NOT NULL ,TIME DATETIME )");
+        $this->pdo->exec("CREATE TABLE IF NOT EXISTS WHITELIST (PRIMARY KEY (TYPE ,VALUE ),TYPE ENUM('XUID','IP') NOT NULL ,VALUE VARCHAR(20) NOT NULL ,REASON VARCHAR(999) NOT NULL ,TIME DATETIME )");
     }
 }
