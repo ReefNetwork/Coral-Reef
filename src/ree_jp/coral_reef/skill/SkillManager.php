65648c97e425a01c955e6a878a9a71bb555e4e16
Kondo Ryosuke <mail@ree-jp.net>
2021-11-05 01:22:21 +0900

    音量を1に

--- src/ree_jp/coral_reef/skill/SkillManager.php
+++ src/ree_jp/coral_reef/skill/SkillManager.php
@@ -81,8 +81,7 @@ class SkillManager
             CoralReefPlugin::$plugin->getScheduler()->scheduleDelayedTask(new ClosureTask(function (int $currentTick) use ($xuid, $p): void {
                 $p->sendPopup('スキルのクールタイムが終了しました');
                 AccountManager::setValue($xuid, 'skill_cool_time', 0);
-                $volume = 0x10000000 * (min(30, $currentTick) / 5);
-                $p->getLevel()->broadcastLevelSoundEvent($p, LevelSoundEventPacket::SOUND_LEVELUP, (int)$volume);
+                $p->getLevel()->broadcastLevelSoundEvent($p, LevelSoundEventPacket::SOUND_LEVELUP, 1);
             }), $cool_time);
         }
     }
