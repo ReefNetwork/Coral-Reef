7e335e25fbc36ab2c01a9912cf16fc0163d72cd8
Kondo Ryosuke <mail@ree-jp.net>
2021-11-06 23:54:32 +0900

    docsを追加

--- build/make-phar.php
+++ build/make-phar.php
@@ -15,7 +15,7 @@ if (file_exists($file_phar)) {
 $files = [];
 $dir = getcwd() . DIRECTORY_SEPARATOR;
 
-$exclusions = ["github", ".gitignore", "composer.json", "composer.lock", "build", ".git", ".idea", "LICENCES", "deploy.bat", "docker-compose.yml", "setup"];
+$exclusions = ["github", ".gitignore", "composer.json", "composer.lock", "build", ".git", ".idea", "LICENCES", "deploy.bat", "docker-compose.yml", "docs", "setup"];
 
 foreach (new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($dir)) as $path => $file) {
     $bool = true;
