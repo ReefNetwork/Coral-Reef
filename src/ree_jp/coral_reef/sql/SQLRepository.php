aa1099b21ef05191d21ad2067c6e5b223ad0b480
たくまっち   Takumachi <47268002+xtakumatutix@users.noreply.github.com>
2022-04-17 14:23:35 +0900

    色付け

--- src/ree_jp/coral_reef/sql/SQLRepository.php
+++ src/ree_jp/coral_reef/sql/SQLRepository.php
@@ -196,20 +196,20 @@ class SQLRepository
     public function getWarps(string $xuid, ?Closure $func): void
     {
         $this->db->executeSelect('coral_reef.warp.get', ['xuid' => intval($xuid), 'server' => CoralReefPlugin::$serverID],
-            $func, $this->noticeByXUid($xuid, 'エラーが発生しました'));
+            $func, $this->noticeByXUid($xuid, '§cエラーが発生しました'));
     }
 
     public function addWarp(string $xuid, string $name, string $level, int $x, int $y, int $z): void
     {
         $this->db->executeInsert('coral_reef.warp.create',
             ['xuid' => intval($xuid), 'name' => $name, 'server' => CoralReefPlugin::$serverID, 'level' => $level, 'x' => $x, 'y' => $y, 'z' => $z],
-            $this->noticeByXUid($xuid, 'ワープ地点を作成しました'), $this->noticeByXUid($xuid, 'エラーが発生しました'));
+            $this->noticeByXUid($xuid, '§aワープ地点を作成しました'), $this->noticeByXUid($xuid, '§cエラーが発生しました'));
     }
 
     public function deleteWarp(string $xuid, string $name): void
     {
         $this->db->executeGeneric('coral_reef.warp.delete', ['xuid' => intval($xuid), 'name' => $name, 'server' => CoralReefPlugin::$serverID],
-            $this->noticeByXUid($xuid, 'ワープ地点を削除しました'), $this->noticeByXUid($xuid, 'エラーが発生しました'));
+            $this->noticeByXUid($xuid, '§aワープ地点を削除しました'), $this->noticeByXUid($xuid, '§cエラーが発生しました'));
     }
 
     public function loadProtectLand(Closure $func, Closure $failure): void
