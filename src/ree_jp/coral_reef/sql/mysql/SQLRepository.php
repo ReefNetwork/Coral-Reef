8a8de34d1cc8798a744538529619809bd47bf4e1
ree-jp <mail@ree-jp.net>
2022-05-22 13:57:46 +0900

    実行されないから悲しくなる [deploy]

--- src/ree_jp/coral_reef/sql/mysql/SQLRepository.php
+++ src/ree_jp/coral_reef/sql/mysql/SQLRepository.php
@@ -57,8 +57,9 @@ class SQLRepository implements Repository
 
         // ユーザーデータを読み込む
         $this->accountStore->setValue($xuid, "wait_action");
+        $p->setImmobile();
         $await = [];
-        $await[] = Await::promise(fn($func) => $this->pool->getConnection()->executeSelect('coral_reef.user.get', ['xuid' => intval($p->getXuid())],
+        $await[] = fn() => Await::promise(fn($func) => $this->pool->getConnection()->executeSelect('coral_reef.user.get', ['xuid' => intval($p->getXuid())],
             function (array $rows) use ($func, $p) {
                 if (!$p->isOnline()) return;
 
@@ -79,7 +80,7 @@ class SQLRepository implements Repository
                 }
                 $func();
             }));
-        $await[] = Await::promise(fn($func) => $this->getAllSubtypeValue($xuid, SQLConst::TYPE_SETTINGS,
+        $await[] = fn() => Await::promise(fn($func) => $this->getAllSubtypeValue($xuid, SQLConst::TYPE_SETTINGS,
             function (array $rows) use ($func, $xuid) {
                 foreach ($rows as $option) {
                     if (array_key_exists("subtype", $option) && array_key_exists("value", $option)) {
@@ -90,7 +91,7 @@ class SQLRepository implements Repository
                 }
                 $func();
             }));
-        $await[] = AccountService::loadPlayerData($this->pool, $p);
+        $await[] = fn() => AccountService::loadPlayerData($this->pool, $p);
         Await::all($await);
         if (!$p->isConnected()) return;
         $p->sendMessage("データを読み込みました");
