a502281d67c768007b499d9bbe1fb2ebbddac2e6
ree-jp <mail@ree-jp.net>
2022-08-01 10:35:38 +0900

    :ambulance: 土地保護関連

--- src/ree_jp/coral_reef/sql/mysql/MysqlLandRepo.php
+++ src/ree_jp/coral_reef/sql/mysql/MysqlLandRepo.php
@@ -34,7 +34,7 @@ class MysqlLandRepo implements LandRepository
     public function getLands(string $server): Generator
     {
         $result = yield from Await::promise(
-            fn($resolve, $reject) => $this->pool->getConnection()->executeInsert("coral_reef.land.get",
+            fn($resolve, $reject) => $this->pool->getConnection()->executeSelect("coral_reef.land.get",
                 ["server" => $server], $resolve, $reject));
         if (!$result) return [];
 
