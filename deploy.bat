2e941fcf0784d9280f171da6487a4cdc682b1217
ree-jp <mail@ree-jp.net>
2022-08-05 14:20:26 +0900

    :fire: 要らないの物を整理

--- deploy.bat
+++
@@ -1,3 +0,0 @@
-php -dphar.readonly=0 ./build/make-phar.php enableCompressAll
-copy /y build\CoralReef.phar D:\docker\pmmp\plugins
-docker run -it --rm -v D:/docker/pmmp/plugins:/plugins -v D:/docker/pmmp:/data -v D:/docker/pmmp:/pocketmine -p 19132:19132/udp pmmp/pocketmine-mp:latest
