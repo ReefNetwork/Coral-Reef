0fdec066468e31a9a9f3fed58843dcabc4d53a86
ree-jp <mail@ree-jp.net>
2022-12-10 19:00:30 +0900

    :zap: ガチャ選択のテスト8 [deploy]

--- src/ree_jp/coral_reef/form/gatya/GatyaForm.php
+++ src/ree_jp/coral_reef/form/gatya/GatyaForm.php
@@ -58,8 +58,8 @@ class GatyaForm
                     })
                 );
             foreach (self::NOW_GATYA as $gatya) {
-                var_dump(array_rand(GatyaService::GATYA[$gatya]["pick_up_image"]));
-                $form->addElement(new ClosureButton("[gatya_select]", new ButtonImage(ButtonImage::TYPE_PATH, "textures/items/reef_pickaxe"),
+                $randImage = array_rand(GatyaService::GATYA[$gatya]["pick_up_image"]);
+                $form->addElement(new ClosureButton("[gatya_select]", new ButtonImage(ButtonImage::TYPE_PATH, $randImage),
                     function () use ($repo, $p, $gatya): void {
                         self::sendForm($repo, $p, $gatya);
                     }
