480cd5100ed536bd42400c1a8df5f4ddc1ae7ec8
Ree-jp <mail@ree-jp.net>
2022-12-31 18:34:02 +0900

    :tada: 2023

--- src/ree_jp/coral_reef/form/menu/BonusCodeForm.php
+++ src/ree_jp/coral_reef/form/menu/BonusCodeForm.php
@@ -58,6 +58,15 @@ class BonusCodeForm
                     GatyaManager::addTicket($repo, $p->getXuid(), SQLConst::TICKETS_CHRISTMAS_2022, 10);
                 });
                 break;
+            case "2023":
+                self::useCode($repo, $p, $code, function () use ($repo, $p): void {
+                    $p->sendMessage("あけおめ！！！");
+                    $p->sendMessage(TextFormat::AQUA . "ガチャチケットを" . TextFormat::RED . "2枚" . TextFormat::AQUA . "受け取りました");
+                    GatyaManager::addTicket($repo, $p->getXuid(), SQLConst::TICKETS_NORMAL, 2);
+                    $p->sendMessage(TextFormat::RED . "クリスマス" . TextFormat::RESET . "ガチャチケットを" . TextFormat::RED . "3枚" . TextFormat::AQUA . "受け取りました");
+                    GatyaManager::addTicket($repo, $p->getXuid(), SQLConst::TICKETS_CHRISTMAS_2022, 3);
+                });
+                break;
             default:
                 $p->sendMessage("そのコードは存在しません");
         }
