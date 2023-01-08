739d349e4222c794442f859a862c47fe0a503397
ree-jp <mail@ree-jp.net>
2023-01-08 20:29:25 +0900

    :fire: クリスマスイベント終わり

--- src/ree_jp/coral_reef/quest/QuestManager.php
+++ src/ree_jp/coral_reef/quest/QuestManager.php
@@ -15,8 +15,6 @@ use Closure;
 use ree_jp\coral_reef\account\AccountStore;
 use ree_jp\coral_reef\quest\data\DailyDigQuest;
 use ree_jp\coral_reef\quest\data\DailyLoginQuest;
-use ree_jp\coral_reef\quest\data\event\christmas_2022\Christmas2022DailyDig;
-use ree_jp\coral_reef\quest\data\event\christmas_2022\Christmas2022DailyLogin;
 use ree_jp\coral_reef\quest\data\LevelUpQuest;
 use ree_jp\coral_reef\quest\data\QuestData;
 use ree_jp\coral_reef\quest\data\TutorialQuest;
@@ -46,8 +44,8 @@ class QuestManager
             self::addUserQuest($repo, $xuid, WeeklyAchieveQuest::ID, null);
 
             // 期間限定
-            self::addUserQuest($repo, $xuid, Christmas2022DailyLogin::ID, null);
-            self::addUserQuest($repo, $xuid, Christmas2022DailyDig::ID, null);
+//            self::addUserQuest($repo, $xuid, Christmas2022DailyLogin::ID, null);
+//            self::addUserQuest($repo, $xuid, Christmas2022DailyDig::ID, null);
 
             if ($func instanceof Closure) $func($rows);
         });
@@ -77,8 +75,8 @@ class QuestManager
             WeeklyAchieveQuest::ID => new WeeklyAchieveQuest($repo, $xuid, $value),
 
             // 期間限定
-            Christmas2022DailyLogin::ID => new Christmas2022DailyLogin($repo, $xuid, $value),
-            Christmas2022DailyDig::ID => new Christmas2022DailyDig($repo, $xuid, $value),
+//            Christmas2022DailyLogin::ID => new Christmas2022DailyLogin($repo, $xuid, $value),
+//            Christmas2022DailyDig::ID => new Christmas2022DailyDig($repo, $xuid, $value),
 
             default => null,
         };
--- src/ree_jp/coral_reef/session/SessionService.php
+++ src/ree_jp/coral_reef/session/SessionService.php
@@ -14,12 +14,9 @@ namespace ree_jp\coral_reef\session;
 use Generator;
 use pocketmine\Server;
 use ree_jp\coral_reef\CoralReefPlugin;
-use ree_jp\coral_reef\gatya\GatyaManager;
 use ree_jp\coral_reef\sql\model\BlockStatisticsModel;
-use ree_jp\coral_reef\sql\mysql\SQLRepository;
 use ree_jp\coral_reef\sql\repo\SessionRepository;
 use ree_jp\coral_reef\sql\RepositoryPool;
-use ree_jp\coral_reef\sql\SQLConst;
 use ree_jp\coral_reef\StoreHouse;
 use ree_jp\reef_edge\ReefEdgePlugin;
 use ree_jp\reef_edge\socket\SocketService;
@@ -73,8 +70,8 @@ class SessionService
                 }
             }
 
-            /** @var SQLRepository $sqlRepo */
-            $sqlRepo = $pool->get(SQLRepository::class);
+//            /** @var SQLRepository $sqlRepo */
+//            $sqlRepo = $pool->get(SQLRepository::class);
 
             $message = "---" . round((time() - $measureTime) / 60, 1) . "分の整地ランキング---(" . CoralReefPlugin::$serverDisplay . "サーバー)---\n";
             $now = 1;
@@ -83,14 +80,14 @@ class SessionService
                 if ($now > 5) break;
                 $number = 0;
                 foreach ($names as $name) {
-                    $getTicket = 6 - $now;
-                    $p = Server::getInstance()->getPlayerExact($name);
-                    if (is_null($p)) {
-                        $message .= "$name さんが見つかりませんでした";
-                    } else {
-                        GatyaManager::addTicket($sqlRepo, $p->getXuid(), SQLConst::TICKETS_CHRISTMAS_2022, $getTicket);
-                        $p->sendMessage("ランキング報酬として§cクリスマス§rガチャチケットを$getTicket 枚受け取りました");
-                    }
+//                    $getTicket = 6 - $now;
+//                    $p = Server::getInstance()->getPlayerExact($name);
+//                    if (is_null($p)) {
+//                        $message .= "$name さんが見つかりませんでした";
+//                    } else {
+//                        GatyaManager::addTicket($sqlRepo, $p->getXuid(), SQLConst::TICKETS_CHRISTMAS_2022, $getTicket);
+//                        $p->sendMessage("ランキング報酬として§cクリスマス§rガチャチケットを$getTicket 枚受け取りました");
+//                    }
 
                     $message .= "$now 位 $name さん($count)\n";
                     $number++;
